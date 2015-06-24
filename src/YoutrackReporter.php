bd0c98c42a444d1e21171d01bc6016d5045cbef2
Paul «Angmar» Drozd <paul@drozd.in.ua>
2015-06-24 15:22:29 +0300

    YouTrack support

---
+++ src/YoutrackReporter.php
@@ -0,0 +1,162 @@
+<?php
+
+use YouTrack\Connection as YouTrackConnection;
+
+class YoutrackReporter implements BugReporter
+{
+
+    protected $asana;
+    protected $projectName;
+    protected $projectId;
+    protected $user;
+    protected $password;
+    protected $client;
+
+    public function __construct($baseUrl, $username, $password, $projectName = null)
+    {
+
+        $this->baseUrl = $baseUrl;
+
+        $this->client = new YouTrackConnection($baseUrl, $username, $password);
+
+        if ($projectName) {
+            $this->projectName = $projectName;
+            $this->projectId   = $this->getProjectIdByName($projectName);
+        }
+
+    }
+
+    /**
+     * Get type list
+     *
+     * @return array
+     */
+    public function getTypes()
+    {
+        return array(
+            'Bug',
+            'Cosmetics',
+            'Exception',
+            'Feature',
+            'Task',
+            'Usability Problem',
+            'Performance Problem',
+            'Epic',
+            'Meta issue',
+            'Auto-reported exception'
+        );
+    }
+
+    /**
+     * Creates a new issue with the given title and description.
+     *
+     * @param $summary
+     * @param $description
+     * @return array
+     */
+    public function createIssue($title, $body)
+    {
+        $issue = $this->client->createIssue($this->projectId, $title, array('description' => $body));
+        return $issue->getAsArray();
+    }
+
+    /**
+     * Update an issue with a new description.
+     *
+     * @param $key
+     * @param $id
+     * @param $description
+     * @return boolean
+     */
+    public function editIssue($key, $id, $body)
+    {
+        $issue = $this->getSummary(null, $id);
+        $issue = $this->client->updateIssue($id, $issue['summary'], $body);
+        return $issue->getAsArray();
+    }
+
+    /**
+     * List all issues of this project in the bug system.
+     *
+     * @return array
+     */
+    public function getIssues()
+    {
+        return array_map(function ($issue) {
+            return $issue->id;
+        }, $this->client->getIssues($this->projectId, null, null, null));
+    }
+
+    /**
+     * Get issue endpoint url
+     *
+     * @param IssueWrapper $issue
+     * @return string
+     */
+    public function getIssueEndpointUrl(IssueWrapper $issue)
+    {
+
+    }
+
+    /**
+     * Returns the status of a specific issue.
+     *
+     * @param $issueKey
+     * @param $id
+     * @return string
+     */
+    public function getStatus($key, $id)
+    {
+        $issue = $this->getSummary(null, $id);
+        return (!empty($issue['State'])) ? $issue['State'] : null;
+    }
+
+    /**
+     * Returns the summary of a specific issue.
+     *
+     * @param $issueKey
+     * @param $id
+     * @return string
+     */
+    public function getSummary($key, $id)
+    {
+        return $this->client->getIssue($id)->getAsArray();
+    }
+
+    /**
+     * Returns the description of a specific issue.
+     *
+     * @param $key
+     * @param $id
+     * @return string
+     */
+    public function getDescription($key, $id)
+    {
+        $issue = $this->getSummary(null, $id);
+        return (!empty($issue['description'])) ? $issue['description'] : null;
+    }
+
+    /**
+     * Get available projects.
+     *
+     * @return array
+     */
+    public function getProjects()
+    {
+
+        return array_map(function ($project) {
+            return $project->name;
+        }, $this->client->getAccessibleProjects());
+
+    }
+
+    protected function getProjectIdByName($projectName)
+    {
+        foreach ($this->client->getAccessibleProjects() as $project) {
+            if ($project->name == $projectName) {
+                return $project->getShortName();
+            }
+        }
+        return false;
+    }
+}
---
+++ tests/YoutrackReporterTest.php
@@ -0,0 +1,96 @@
+<?php
+
+class YoutrackReporterTest extends PHPUnit_Framework_TestCase
+{
+
+    protected $project  = 'Test project 1';
+
+    // protected $url      = 'http://localhost:1234';
+    // protected $username = 'root';
+    // protected $password = '123456';
+
+    protected $url      = 'https://angmarian.myjetbrains.com/youtrack';
+    protected $username = 'root';
+    protected $password = '25179long';
+
+    public function testReporter()
+    {
+
+        $faker = Faker\Factory::create();
+
+        $reporter = new YoutrackReporter($this->url, $this->username, $this->password, $this->project);
+
+        $this->assertNotFalse($reporter);
+
+
+        // testing issue creation
+
+        $issueTitle = $faker->text(100);
+        $issueBody  = $faker->text(400);
+
+        $issue = $reporter->createIssue($issueTitle, $issueBody);
+
+        $this->assertNotFalse($issue);
+
+        $this->assertNotEmpty($issue['summary']);
+
+        $this->assertEquals($issue['summary'], $issueTitle);
+
+        $issueId = $issue['id'];
+
+
+        // testing getTypes
+
+        $this->assertInternalType('array', $reporter->getTypes());
+
+
+        // testing issues loading
+
+        $issues = $reporter->getIssues();
+
+        $this->assertNotEmpty($issues);
+
+
+        // looking for newly created issue
+
+        $found = false;
+        foreach ($issues as $issue) {
+            if ($issue == $issueId) $found = true;
+        }
+
+        $this->assertTrue($found);
+
+
+        // checking status
+
+        $issue = $reporter->getSummary(null, $issueId);
+
+        $this->assertNotEmpty($issue);
+
+        $this->assertEquals($reporter->getStatus(null, $issueId), 'Submitted');
+
+
+        // check original content
+
+        $this->assertEquals($issue['summary'], $issueTitle);
+
+        $description = $reporter->getDescription(null, $issueId);
+
+        $this->assertEquals($description, $issueBody);
+
+
+        // updating issue
+
+        $newBody = $faker->text(500);
+
+        $reporter->editIssue(null, $issueId, $newBody);
+
+        $issue = $reporter->getSummary(null, $issueId);
+
+        $this->assertEquals($issue['description'], $newBody);
+
+        $this->assertNotEquals($issue['description'], $issueBody);
+
+    }
+
+}
